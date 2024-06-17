08445a15fe69e9e11b65ad2a58bce7e3f6cae5b3
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-06-17 23:07:10 +0300

    modify page.tsx file

--- next-auth-app/app/page.tsx
+++ next-auth-app/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <div className="font-bold">Hello world</div>
+    <>
+      <div className="font-bold m-3">Hello world</div>
+      <Button className="ml-3">Click me</Button>
+    </>
   );
 }
