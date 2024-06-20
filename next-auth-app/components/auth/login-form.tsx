553a3a5bb70fc55833a10a1de773a1a969d10375
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-06-20 11:31:03 +0300

    add login schema

--- next-auth-app/components/auth/login-form.tsx
+++ next-auth-app/components/auth/login-form.tsx
@@ -1,6 +1,29 @@
-import { CardWrapper } from "./card-wrapper"
+"use client";
+
+import * as z from "zod";
+
+import { CardWrapper } from "./card-wrapper";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form"
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: ""
+    }
+  });
+
   return (
     <CardWrapper
       headerLabel="Welcome back"
