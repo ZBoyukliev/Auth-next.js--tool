272b5a23bc9b2d1d1d594563cc1a78009a334f47
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-06-22 18:42:30 +0300

    common changes

--- next-auth-app/auth.config.ts
+++ next-auth-app/auth.config.ts
@@ -2,7 +2,18 @@ import GitHub from "next-auth/providers/github"
 import type { NextAuthConfig } from "next-auth";
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "./lib/db"
+;
+ const config = {
+    providers: [GitHub],
+    adapter: PrismaAdapter(db),
+    session: { strategy: 'jwt' },
+} satisfies NextAuthConfig;
 
-export const config = { providers: [GitHub] } satisfies NextAuthConfig;
 
-export const { auth, handlers, signIn, signOut } = NextAuth(config)
+export const {
+    auth,
+    handlers,
+    signIn,
+    signOut
+} = NextAuth(config)
