8524c9dae2f4f274712760d677a4e402c5207df1
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-06-25 16:15:37 +0300

    common changes

--- next-auth-app/auth.ts
+++ next-auth-app/auth.ts
@@ -1,19 +1,30 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { getUserById } from "./data/user";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   callbacks: {
     async session({ token, session, user }) {
-      console.log({ sessionToken: token, session })
+      console.log({ sessionToken: token})
       if (token.sub && session.user) {
         session.user.id = token.sub;
+      };
+
+      if(token.role && session.user) {
+        session.user.role = token.role;
       }
 
       return session
     },
     async jwt({ token }) {
+
+      if(!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+
+      if(!existingUser) return token;
+      token.role = existingUser.role;
       return token;
     }
   },
