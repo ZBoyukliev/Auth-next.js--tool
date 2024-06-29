73e94337755127e8c9c94b33fcda95e11f6f1df1
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-06-29 12:35:19 +0300

    set up login with google and github

--- next-auth-app/auth.config.ts
+++ next-auth-app/auth.config.ts
@@ -5,11 +5,17 @@ import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-;
+
 export default {
     providers: [
-        Google,
-        Github,
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }),
+        Github({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
+        }),
         Credentials({
             async authorize(credentials, req) {
 
