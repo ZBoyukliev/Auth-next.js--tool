44afe64272ee0ca98ead51405f3be9f312c17ed4
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-06-29 12:45:01 +0300

    add verify email functionality to oauth login

--- next-auth-app/auth.ts
+++ next-auth-app/auth.ts
@@ -6,6 +6,18 @@ import { db } from "./lib/db";
 import { UserRole } from "@prisma/client";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error"
+  },
+  events: {
+    async linkAccount({user}){
+      await db.user.update({
+        where: {id: user.id},
+        data: { emailVerified: new Date()}
+      })
+    }
+  },
   callbacks: {
     async session({ token, session, user }) {
       if (token.sub && session.user) {
