94710fbe363b928864c271dc433fb733fda300ff
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-07-10 13:47:39 +0300

    common

--- next-auth-app/actions/login.ts
+++ next-auth-app/actions/login.ts
@@ -5,6 +5,8 @@ import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
+import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
@@ -14,9 +16,21 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     };
 
     const { email, password } = validatedFields.data;
+    const existingUser = await getUserByEmail(email);
+
+    if (!existingUser || !existingUser.email || !existingUser.password) {
+        return { error: "Email does not exist!" }
+    };
+
+    if (!existingUser.emailVerified) {
+        const verificationToken = await generateVerificationToken(
+            existingUser.email
+        );
+        return { success: "Confirmation email sent!"}
+    }
 
     try {
-       await signIn("credentials", {
+        await signIn("credentials", {
             email,
             password,
             redirectTo: DEFAULT_LOGIN_REDIRECT
