1f4b40fff99c17229afac4e3b6a526b823acb6a4
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-07-16 22:54:37 +0300

    format the code

--- next-auth-app/actions/register.ts
+++ next-auth-app/actions/register.ts
@@ -7,6 +7,7 @@ import {db} from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -32,8 +33,11 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         }
     });
 
-     //TODO: Send verification token email!
      const verificationToken = await generateVerificationToken(email);
+     await sendVerificationEmail(
+        verificationToken.email,
+        verificationToken.token
+     )
 
     return {success: "Confirmation email sent!"}
 }
