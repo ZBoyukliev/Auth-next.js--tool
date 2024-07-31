e9969d6d4de85a4eb246e633de29d499cab4c724
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-07-31 12:37:14 +0300

    create email verificationerrors

--- next-auth-app/components/auth/new-verification-form.tsx
+++ next-auth-app/components/auth/new-verification-form.tsx
@@ -3,32 +3,51 @@
 import { useSearchParams } from "next/navigation"
 import { CardWrapper } from "./card-wrapper"
 import { BeatLoader } from "react-spinners"
-import { useCallback, useEffect } from "react"
+import { useCallback, useEffect, useState } from "react"
+import { newVerification } from "@/actions/new-verification"
+import { FormSuccess } from "../form-success"
+import { FormError } from "../form-error"
 
 const NewVerificationForm = () => {
-    const searchParams = useSearchParams()
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+  const searchParams = useSearchParams()
 
-    const token = searchParams.get('token')
+  const token = searchParams.get('token')
 
-    const onSubmit = useCallback(() => {
-        console.log(token)
-    }, [token])
+  const onSubmit = useCallback(() => {
+    if (!token) {
+      setError("Missing token!")
+      return
+    }
 
-    useEffect(() => {
-        onSubmit()
-    },[onSubmit])
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success)
+        setError(data.error)
+      })
+      .catch(() => {
+        setError("Something went wrong!")
+      })
+  }, [token])
 
-    return (
-      <CardWrapper
+  useEffect(() => {
+    onSubmit()
+  }, [onSubmit])
+
+  return (
+    <CardWrapper
       headerLabel="Confirm your verification"
       backButtonLabel="Back to login"
       backButtonHref="/auth/login"
-      >
-        <div className="flex items-center w-full justify-center">
-        <BeatLoader/>
-        </div>
-      </CardWrapper>
-    )
-  }
-  
-  export default NewVerificationForm
+    >
+      <div className="flex items-center w-full justify-center">
+        <BeatLoader />
+        <FormSuccess message={success}/>
+        <FormError message={error}/>
+      </div>
+    </CardWrapper>
+  )
+}
+
+export default NewVerificationForm
