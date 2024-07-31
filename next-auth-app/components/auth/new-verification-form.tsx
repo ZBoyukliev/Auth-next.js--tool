93d2aa9bb854822ee7e83046956f527e2f4e67d4
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-07-31 12:28:26 +0300

    create email verification action

--- next-auth-app/components/auth/new-verification-form.tsx
+++ next-auth-app/components/auth/new-verification-form.tsx
@@ -1,8 +1,23 @@
 "use client"
 
+import { useSearchParams } from "next/navigation"
 import { CardWrapper } from "./card-wrapper"
+import { BeatLoader } from "react-spinners"
+import { useCallback, useEffect } from "react"
 
 const NewVerificationForm = () => {
+    const searchParams = useSearchParams()
+
+    const token = searchParams.get('token')
+
+    const onSubmit = useCallback(() => {
+        console.log(token)
+    }, [token])
+
+    useEffect(() => {
+        onSubmit()
+    },[onSubmit])
+
     return (
       <CardWrapper
       headerLabel="Confirm your verification"
@@ -10,7 +25,7 @@ const NewVerificationForm = () => {
       backButtonHref="/auth/login"
       >
         <div className="flex items-center w-full justify-center">
-
+        <BeatLoader/>
         </div>
       </CardWrapper>
     )
