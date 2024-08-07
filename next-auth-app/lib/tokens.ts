b5713cd51f8baa7d92e3ec636b6fb8ea976e7db5
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-08-07 13:23:59 +0300

    create reset token functionality

--- next-auth-app/lib/tokens.ts
+++ next-auth-app/lib/tokens.ts
@@ -1,6 +1,32 @@
 import { getVerificationTokenMyEmail } from "@/data/verification-token";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "./db";
+import { getResetTokenByEmail } from "@/data/password-reset-token";
+
+export const generatePasswordResetToken = async (email: string) => {
+    const token = uuidv4();
+    const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+    const existingToken = await getResetTokenByEmail(email);
+
+    if (existingToken) {
+        await db.passwordResetToken.delete({
+            where: {
+                id: existingToken.id
+            }
+        })
+    };
+
+    const passwordResetTokenToken = await db.passwordResetToken.create({
+        data: {
+            email,
+            token,
+            expires
+        }
+    });
+
+    return passwordResetTokenToken;
+}
 
 export const generateVerificationToken = async (email: string) => {
     const token = uuidv4();
