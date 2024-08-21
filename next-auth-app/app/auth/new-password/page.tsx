30f175d0fbc1d86a24c9de5ad53352792a4e0f90
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-08-21 19:56:12 +0300

    create new-password form

--- next-auth-app/app/auth/new-password/page.tsx
+++ next-auth-app/app/auth/new-password/page.tsx
@@ -1,7 +1,8 @@
+import { NewPasswordForm } from "@/components/auth/new-password-form"
 
 const NewPasswordPage = () => {
   return (
-    <div>NewPasswordPage</div>
+    <NewPasswordForm />
   )
 }
 
