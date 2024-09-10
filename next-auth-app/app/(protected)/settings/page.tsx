30794f9ef879839eb35e40e2001133c460202eff
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-09-10 19:50:06 +0300

    change settings page to client component0

--- next-auth-app/app/(protected)/settings/page.tsx
+++ next-auth-app/app/(protected)/settings/page.tsx
@@ -1,15 +1,13 @@
-import { auth, signOut } from "@/auth";
+"use client"
 import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
 
-const SettingsPage = async () => {
-    const session = await auth();
+const SettingsPage =  () => {
+    const session = useSession();
     return (
         <>
             <div>{JSON.stringify(session)}</div>
-            <form action={async () => {
-                "use server"
-                await signOut()
-            }}>
+            <form>
                 <Button type="submit">
                     Signout
                 </Button>
