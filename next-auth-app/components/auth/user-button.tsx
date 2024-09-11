acec5c162abca9edd246d2efe861835319d9b655
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-09-11 21:44:30 +0300

    finish navbar

--- next-auth-app/components/auth/user-button.tsx
+++ next-auth-app/components/auth/user-button.tsx
@@ -2,12 +2,33 @@
 
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
+import { FaUser } from "react-icons/fa";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { LogoutButton } from "./logout-buton";
+import { ExitIcon } from "@radix-ui/react-icons";
 
 export const UserButton = () => {
 
+    const user = useCurrentUser();
+
     return(
-        <div>
-            User Button
-        </div>
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                    <AvatarImage src={user?.image || ""} />
+                    <AvatarFallback className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+                        <FaUser className="text-white"/>
+                    </AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-40" align="end">
+                <LogoutButton>
+                    <DropdownMenuItem className="cursor-pointer">
+                        <ExitIcon className="w-4 h-4 mr-3"/>
+                        Logout
+                    </DropdownMenuItem>
+                </LogoutButton>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
