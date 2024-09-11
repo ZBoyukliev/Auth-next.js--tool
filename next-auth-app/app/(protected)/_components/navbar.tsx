53ce83b17c44f350a7ab29b4333db734d354881f
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-09-11 20:59:04 +0300

    style the navbar

--- next-auth-app/app/(protected)/_components/navbar.tsx
+++ next-auth-app/app/(protected)/_components/navbar.tsx
@@ -11,7 +11,14 @@ export const Navbar = () => {
     return (
         <nav className="bg-secondary flex justify-between p-4 items-center rounded-xl w-[600px] shadow-sm">
             <div className="flex gap-x-2">
-                <Button asChild>
+                <Button asChild variant={pathName === "/server" ? "default" : "outline"}>
+                    <Link href="/server">Server</Link>
+                </Button><Button asChild variant={pathName === "/client" ? "default" : "outline"}>
+                    <Link href="/client">Client</Link>
+                </Button><Button asChild variant={pathName === "/admin" ? "default" : "outline"}>
+                    <Link href="/admin">Admin</Link>
+                </Button>
+                <Button asChild variant={pathName === "/settings" ? "default" : "outline"}>
                     <Link href="/settings">Settings</Link>
                 </Button>
             </div>
