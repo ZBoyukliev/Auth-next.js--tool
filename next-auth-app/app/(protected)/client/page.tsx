f733458432d700ff376ed4d37a2e050e95f9ee2d
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-09-12 22:43:41 +0300

    style the client component

---
+++ next-auth-app/app/(protected)/client/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { UserInfo } from "@/components/user-info";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { currentUser } from "@/lib/auth"
+
+const ClientPage =  () => {
+
+    const user = useCurrentUser();
+    return (
+        <UserInfo label="Client Component" user={user} />
+    )
+}
+
+export default ClientPage
