952a138433c0b90303fefb7111093767ecbad0ca
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-09-29 21:52:59 +0300

    finish oauth project

--- next-auth-app/app/(protected)/layout.tsx
+++ next-auth-app/app/(protected)/layout.tsx
@@ -10,7 +10,7 @@ const ProtectedLayout = async({ children }: ProtectedLayoutProps) => {
     const session = await auth();
     return (
         <SessionProvider session={session}>
-            <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+            <div className="min-h-screen p-14 w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
                 <Navbar/>
                 {children}
             </div>
